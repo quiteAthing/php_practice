92b0e20c0c37ad985b3a0889c066e4f5994a5b26
J004G2-H <this@isnotreal.com>
2017-05-05 15:08:33 +0800

    af

--- prac4.php
+++ prac4.php
@@ -7,7 +7,7 @@
 	$password='';
 	
 	try{
-		$conn=new PDO('mysql:host=$host_name;dbname=$dbname',$username,$password);
+		$conn=new PDO('mysql:host=$host_name;dbname=xblog',$username,$password);
 		$conn->setAttribute(PDO::ATTR_ERRORMODE,PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e )
