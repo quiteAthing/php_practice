3e12fc64b463c2b04a11abc9512fec8566c20556
J004G2-H <this@isnotreal.com>
2017-05-05 15:06:26 +0800

    why

--- prac6.php
+++ prac6.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=xblog", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
