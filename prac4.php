ba5d7759e9d39c391c2e8372c296238b552ee9c8
J004G2-H <this@isnotreal.com>
2017-05-05 13:54:49 +0800

    add pdo phrases

--- prac4.php
+++ prac4.php
@@ -1,8 +1,21 @@
 <?php
 	echo "Hello";
-
-
-
-
-
+	
+	$host_name="localhost";
+	$dbname="xblog";
+	$username="prac@localhost";
+	$password="phppractice";
+	
+	try{
+	$conn=new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+	}
+	catch(PDOException $e )
+	{
+		echo "<br>",$e->getMessage();
+		
+	}
+	$result=$conn->query("select * from members where id=1");
+	echo "result ",$result;
+	
+	$conn->close();
 ?>
