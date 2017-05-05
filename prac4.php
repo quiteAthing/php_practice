1306da304446bf1d8baf979e541ecbf75cab38a7
J004G2-H <this@isnotreal.com>
2017-05-05 14:17:09 +0800

    what

--- prac4.php
+++ prac4.php
@@ -12,10 +12,12 @@
 	catch(PDOException $e )
 	{
 		echo "<br>",$e->getMessage();
+		echo (string)$conn,"<br>"
 		
 	}
 	$result=$conn->query("select * from members where id=1");
 	echo "result ",$result->fetch();
+			echo (string)$conn,"<br>"
 	
 	$conn->db=null;
 ?>
