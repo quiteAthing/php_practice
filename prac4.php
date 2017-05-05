8c402a905c2eeaf9049ed4b87481a9ef17815c1b
J004G2-H <this@isnotreal.com>
2017-05-05 15:21:21 +0800

    be

--- prac4.php
+++ prac4.php
@@ -10,7 +10,7 @@
 		$conn=new PDO("mysql:host=$host_name;dbname=$dbname",$username,$password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$result = $conn->query("select * from members");
-		echo $result->fetch()->alias;
+		echo (string)$result->fetch()
 		
 	}
 	catch(PDOException $e )
