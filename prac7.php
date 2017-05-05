8833cbd3b30f7d4cb5586a3aaf578839cf43fc60
J004G2-H <this@isnotreal.com>
2017-05-05 15:55:18 +0800

    wv

---
+++ prac7.php
@@ -0,0 +1,33 @@
+<?php
+	echo "Hello";
+	
+	$host_name="localhost";
+	$dbname="xblog";
+	$username='root';
+	$password='';
+	
+	try{
+		$conn=new PDO("mysql:host=$host_name;dbname=$dbname",$username,$password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		
+		$result = $conn->query("select * from members");
+		$rq=$result->fetch(PDO::FETCH_ASSOC);
+		
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+		}
+		foreach($rq as $x){
+			echo "<br>",$x ; 
+		}
+		
+	}
+	catch(PDOException $e )
+	{
+		echo "<br>abcd",$e->getMessage();
+
+		
+	}
+	
+	
+	
+?>
