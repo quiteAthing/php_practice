67810debd44b56e03005d0468d669d6f73e9fabb
J004G2-H <this@isnotreal.com>
2017-05-05 15:25:43 +0800

    ku

--- prac4.php
+++ prac4.php
@@ -10,7 +10,6 @@
 		$conn=new PDO("mysql:host=$host_name;dbname=$dbname",$username,$password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$result = $conn->query("select * from members");
-		echo (string)$result->fetch();
 		foreach($result->fetch() as $x){
 			echo <br>,$x;
 		}
