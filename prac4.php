20478667b2e03d194551189a1789b23bb4b33cc3
J004G2-H <this@isnotreal.com>
2017-05-05 14:33:35 +0800

    don t know what to do

--- prac4.php
+++ prac4.php
@@ -12,9 +12,7 @@
 		$stmt->execute();
 		    // set the resulting array to associative
 		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-		foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-			echo $v;
-		}
+		$result->fetchAll();
 		
 		
 	}
